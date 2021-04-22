7247478314bf117294cab90d6cb45ecaf5717126
linesofcode01 <frcoding@outlook.com>
2021-04-22 01:29:31 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contacts from "./contacts.json";
+import { useState } from "react";
 
 function App() {
+  let theContacts = [...contacts];
+  const [showContacts, setShowContacts] = useState(theContacts.splice(0, 5));
+  const [restOfContacts, setRestOfContacts] = useState(theContacts);
+
+  const getRandomContact = () => {
+    let contactIndex = Math.floor(Math.random() * restOfContacts.length);
+    let copyContacts = [...showContacts];
+    let copyRestOfContacts = [...restOfContacts];
+    copyRestOfContacts.splice(contactIndex, 1);
+    copyContacts.push(restOfContacts[contactIndex]);
+    setShowContacts(copyContacts);
+    setRestOfContacts(copyRestOfContacts);
+  };
+
+  const sortName = () => {
+    setShowContacts([
+      ...showContacts.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+      }),
+    ]);
+  };
+
+  const sortPopularity = () => {
+    setShowContacts([
+      ...showContacts.sort((a, b) => {
+        return b.popularity - a.popularity;
+      }),
+    ]);
+  };
+
+  console.log(showContacts, restOfContacts);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getRandomContact}>Add Random Contact</button>
+      <button onClick={sortName}>Name</button>
+      <button onClick={sortPopularity}>Popularity</button>
+      <button>Delete</button>
+      <table>
+        <thread>
+          <tr>
+            <td>Picture</td>
+            <td>Name</td>
+            <td>Popularity</td>
+          </tr>
+        </thread>
+        <tbody>
+          {showContacts.map((eachContact) => {
+            return (
+              <tr>
+                <td>
+                  <img src={eachContact.pictureUrl} />
+                </td>
+                <td>{eachContact.name}</td>
+                <td>{eachContact.popularity}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ src/Table.js
@@ -0,0 +1,10 @@
+const Table = ()
+{contacts.map(eachContact => {
+    return (
+        <tr>
+            <td><img src={eachContact.pictureUrl}></img></td>
+            <td>{eachContact.name}</td>
+            <td>{eachContact.popularity}</td>
+        </tr>
+    )
+})}

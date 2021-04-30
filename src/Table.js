aad00faff6c622b170a470e8ce1042f44a9fb00f
linesofcode01 <frcoding@outlook.com>
2021-04-30 00:51:07 -0700

    update

--- src/Table.js
+++ src/Table.js
@@ -1,10 +1,13 @@
-const Table = ()
-{contacts.map(eachContact => {
+const Table = () => {
+  contacts.map((eachContact) => {
     return (
-        <tr>
-            <td><img src={eachContact.pictureUrl}></img></td>
-            <td>{eachContact.name}</td>
-            <td>{eachContact.popularity}</td>
-        </tr>
-    )
-})}
+      <tr>
+        <td>
+          <img src={eachContact.pictureUrl}></img>
+        </td>
+        <td>{eachContact.name}</td>
+        <td>{eachContact.popularity}</td>
+      </tr>
+    );
+  });
+};
